d6a397704388784a2d2c7c6ea7cc4fc323435ecb
David Murillo <dmz866@hotmail.com>
2025-01-18 15:49:45 -0500

    pokemon logic

--- src/module/pokemon/composables/usePokemonGame.ts
+++ src/module/pokemon/composables/usePokemonGame.ts
@@ -1,20 +1,62 @@
 import { GameStatus } from "@/module/common/enums";
-import { onMounted, ref } from "vue";
+import type { Pokemon, PokemonListResponse } from "@/module/common/interfaces";
+import confetti from 'canvas-confetti';
+import { computed, onMounted, ref } from "vue";
 import { pokemonApi } from "../api/pokemonApi";
-import type { PokemonListResponse } from "@/module/common/interfaces";
 
 export const usePokemonGame = () => {
     const gameStatus = ref<GameStatus>(GameStatus.Playing);
-    const getPokemons = () => {
-        const response = pokemonApi.get<PokemonListResponse>('/?limit=151')
+    const pokemons = ref<Pokemon[]>([]);
+    const pokemonOptions = ref<Pokemon[]>([]);
+    const randomPokemon = computed(() => pokemonOptions.value[Math.floor(Math.random() * pokemonOptions.value.length)]);
+    const isLoading = computed(() => pokemons.value.length === 0);
+    const getPokemons = async () => {
+        const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
+        const pokemonsArray = response.data.results.map((p) => {
+            const urlParts = p.url.split('/');
+
+            return {
+                id: urlParts.at(-2) ?? 0,
+                name: p.name
+            } as Pokemon;
+        });
+
+        return pokemonsArray.sort(() => Math.random() - 0.5);
+    };
+    const getNextOptions = (total: number = 4) => {
+        gameStatus.value = GameStatus.Playing;
+        pokemonOptions.value = pokemons.value.slice(0, total);
+        pokemons.value = pokemons.value.slice(total);
+    };
+    const checkAnswer = (id: number) => {
+        const hasWon = randomPokemon.value.id === id;
+
+        if (hasWon) {
+            gameStatus.value = GameStatus.Won;
+            confetti({
+                particleCount: 300,
+                spread: 150,
+                origin: { y: 0.6 },
+            });
+            return;
+        }
+
+        gameStatus.value = GameStatus.Lost;
     };
 
-    onMounted(() => {
-        getPokemons();
+    onMounted(async () => {
+        pokemons.value = await getPokemons();
+        getNextOptions();
     });
 
     return {
         gameStatus,
-        getPokemons
+        isLoading,
+        pokemonOptions,
+        randomPokemon,
+
+        getPokemons,
+        getNextOptions,
+        checkAnswer,
     };
 };
--- src/module/pokemon/pages/PokemonGame.vue
+++ src/module/pokemon/pages/PokemonGame.vue
@@ -1,14 +1,48 @@
-<script lang="ts" setup>
-import { usePokemonGame } from '../composables/usePokemonGame';
-
-const asd = usePokemonGame();
-</script>
 <template>
-    <section v-if="false" class="w-screen h-screen flex flex-col justify-center items-center">
-        <h1>Espere por favor</h1>
-        <h3 class="animate-pulse">Cargando...</h3>
+    <section
+      v-if="isLoading || randomPokemon.id === null"
+      class="flex flex-col justify-center items-center w-screen h-screen"
+    >
+      <h1 class="text-3xl">Espere por favor</h1>
+      <h3 class="animate-pulse">Cargando Pokémons</h3>
     </section>
-    <section  class="w-screen h-screen flex flex-col justify-center items-center">
+  
+    <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 class="m-5">¿Quién es este Pokémon?</h1>
+  
+      <div class="h-20">
+        <button
+          v-if="gameStatus !== GameStatus.Playing"          
+          class="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 transition-all"
+        >
+          ¿Jugar de nuevo?
+        </button>
+      </div>
 
+      <PokemonPicture
+        :pokemon-id="randomPokemon.id"
+        :show-pokemon="gameStatus !== GameStatus.Playing"
+      />
+      <PokemonOptions
+        :options="options"
+        :block-selection="gameStatus !== GameStatus.Playing"
+        :correct-answer="randomPokemon.id"
+        @selected-option="checkAnswer"
+      />
     </section>
-</template>
+  </template>
+  
+  <script setup lang="ts">
+  import PokemonOptions from '../components/PokemonOptions.vue';
+  import PokemonPicture from '../components/PokemonPicture.vue';
+  import { usePokemonGame } from '../composables/usePokemonGame';
+  import {GameStatus} from '../../common/enums';
+
+  const {
+    randomPokemon,
+    isLoading,
+    gameStatus,
+    pokemonOptions: options,
+    checkAnswer,
+  } = usePokemonGame();
+  </script>
